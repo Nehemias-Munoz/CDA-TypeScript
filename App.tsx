5e84e73719457f0595e7dd1c9fe62376464fa37d
Nehemias-Munoz <nehemiasmarvic.dev@gmail.com>
2021-04-27 11:25:49 -0400

    Limpieza

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import {SafeAreaView, Text, View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import StackNavigation from './src/navigation/StackNavigation';
 const App = () => {
