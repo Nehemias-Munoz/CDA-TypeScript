c0aea67de9597ec04cc2e08e67a15a7762a30a2d
Nehemias-Munoz <nehemiasmarvic.dev@gmail.com>
2021-04-27 11:46:29 -0400

    Terminada apariencia y vinculacion LoginScreen

--- src/navigation/StackNavigation.tsx
+++ src/navigation/StackNavigation.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
+import LoginScreen from '../screens/LoginScreen';
 
 const StackNavigation = () => {
   const Stack = createStackNavigator();
   return (
-    <Stack.Navigator screenOptions={{headerBackTitle: ''}}></Stack.Navigator>
+    <Stack.Navigator screenOptions={{headerBackTitle: ''}}>
+      <Stack.Screen name={'LoginScreen'} component={LoginScreen} />
+    </Stack.Navigator>
   );
 };
 
