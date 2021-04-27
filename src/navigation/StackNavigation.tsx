b430085074ec483ae469f103fb956b4b67bd5ecd
Nehemias-Munoz <nehemiasmarvic.dev@gmail.com>
2021-04-27 12:10:40 -0400

    Terminada apariencia y vinculacion AssignatureS

--- src/navigation/StackNavigation.tsx
+++ src/navigation/StackNavigation.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import LoginScreen from '../screens/LoginScreen';
 import RegisterScreen from '../screens/RegisterScreen';
+import AssignaturesScreen from '../screens/AssignaturesScreen';
 
 const StackNavigation = () => {
   const Stack = createStackNavigator();
@@ -9,6 +10,10 @@ const StackNavigation = () => {
     <Stack.Navigator screenOptions={{headerBackTitle: '', headerTitle: ''}}>
       <Stack.Screen name={'LoginScreen'} component={LoginScreen} />
       <Stack.Screen name={'RegisterScreen'} component={RegisterScreen} />
+      <Stack.Screen
+        name={'AssignaturesScreen'}
+        component={AssignaturesScreen}
+      />
     </Stack.Navigator>
   );
 };
