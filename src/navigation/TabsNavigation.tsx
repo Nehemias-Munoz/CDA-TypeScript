ea88ad900d9786c448d6d93cf9acb6268f1dd971
Nehemias-Munoz <nehemiasmarvic.dev@gmail.com>
2021-04-27 19:24:43 -0400

    Terminada apariencia actividad y vinculacion a Tab

--- src/navigation/TabsNavigation.tsx
+++ src/navigation/TabsNavigation.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
+import ActingGameAct from '../activities/ActingGameAct';
 const TabsNavigation = () => {
   const Tab = createMaterialTopTabNavigator();
-  return <Tab.Navigator></Tab.Navigator>;
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name={'ActingGameAct'} component={ActingGameAct} />
+    </Tab.Navigator>
+  );
 };
 
 export default TabsNavigation;
